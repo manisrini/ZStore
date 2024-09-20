ac4abb1d6a4591e0e5563f3ba2ea934607e06494
manisrini <srinimani678@gmail.com>
2024-09-20 08:34:49 +0530

    add codable for response and form product model

---
+++ ZStore/Codable/HomeScreenResponse.swift
@@ -0,0 +1,41 @@
+//
+//  Products.swift
+//  ZStore
+//
+//  Created by Manikandan on 20/09/24.
+//
+
+import Foundation
+
+struct HomeScreenResponse : Codable{
+    let category : [ProductCategory]?
+    let card_offers : [CardOffer]?
+    let products : [Product]?
+}
+
+struct ProductCategory : Codable{
+    let id : String?
+    let name : String?
+    let layout : String?
+}
+
+struct CardOffer : Codable{
+    let id : String?
+    let percentage : Double?
+    let offer_desc : String?
+    let card_name : String?
+    let max_discount : String?
+    let image_url : String?
+}
+
+struct Product : Codable{
+    let id : String?
+    let name : String?
+    let rating : Double?
+    let review_count : Int?
+    let price : Double?
+    let category_id : String?
+    let card_offer_ids : [String]?
+    let image_url : String?
+    let description : String?
+}
--- ZStore/HomeScreen/HomeScreenViewModel.swift
+++ ZStore/HomeScreen/HomeScreenViewModel.swift
@@ -7,22 +7,86 @@
 
 import Foundation
 
+struct ProductModel{
+    let id : String
+    let name : String
+    let rating : Double?
+    let review_count : Int?
+    let price : Double
+    let category : ProductCategory?
+    let cardOffers : [CardOffer]?
+    let image_url : String?
+    let description : String?
+}
+
+
 class HomeScreenViewModel{
+    
+    var productItems : [ProductModel] = []
+    var availableCategories : [ProductCategory] = []
     
     func fetchData(completion : @escaping() -> Void){
         let url = "https://raw.githubusercontent.com/princesolomon/zstore/main/data.json"
         
         NetworkManager.shared.getData(urlStr: url) { [weak self] data, error in
             guard let _data = data else{ return }
-                    
+            
             do{
-                if let response = try JSONSerialization.jsonObject(with: _data, options: .fragmentsAllowed) as? [String:Any]{
-                    completion()
-                }
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(HomeScreenResponse.self, from: _data)
+                self?.createProductModel(response)
+                completion()
             }catch {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    private func createProductModel(_ response : HomeScreenResponse){
+        self.availableCategories = response.category ?? []
+        
+        var formattedProducts : [ProductModel] = []
+        
+        if let _products = response.products{
+            for product in _products {
+                let category = self.getCategory(product: product, allCategories: response.category ?? [])
+                let cardOffers = self.getCardOffers(product: product, allCardOffers: response.card_offers ?? [])
+                let formattedProduct = ProductModel(
+                    id: product.id ?? "",
+                    name: product.name ?? "",
+                    rating: product.rating,
+                    review_count: product.review_count,
+                    price: product.price ?? 0.0,
+                    category: category,
+                    cardOffers: cardOffers,
+                    image_url: product.image_url,
+                    description: product.description)
+                formattedProducts.append(formattedProduct)
+            }
+        }
+        
+        print(formattedProducts)
+    }
+    
+    private func getCategory(product : Product,allCategories : [ProductCategory]) -> ProductCategory?{
+        let categoryId = product.category_id ?? ""
+        let filteredCategory = allCategories.filter { category in
+            category.id ?? "" == categoryId
+        }.first
+        return filteredCategory
+    }
+    
+    private func getCardOffers(product : Product,allCardOffers : [CardOffer]) -> [CardOffer]?{
+        let currentProductCardOffersIds = product.card_offer_ids ?? []
+        var currentProductOffers : [CardOffer] = []
+        
+        for offer in allCardOffers {
+            let offerId = offer.id ?? ""
+            if currentProductCardOffersIds.contains(offerId){
+                currentProductOffers.append(offer)
+            }
+        }
+        return currentProductOffers
     }
     
 }
